f16036be365c2c4a70773f80e4bf3da4744adc90
annisarhy <analisarahayu@gmail.com>
2020-04-23 21:29:23 +0700

    Transaksi

--- app/src/main/java/com/example/ujikomlaundry/MainActivity.java
+++ app/src/main/java/com/example/ujikomlaundry/MainActivity.java
@@ -61,9 +61,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*public void logout(View view){
+
+
+    public void logout(View view){
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
-    }*/
+    }
 }
--- app/src/main/java/com/example/ujikomlaundry/Splash.java
+++ app/src/main/java/com/example/ujikomlaundry/Splash.java
@@ -2,13 +2,25 @@ package com.example.ujikomlaundry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Register.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
